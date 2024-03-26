fcfd9fdf2794071e2e2953b6b444c115c7980978
luhongran09 <134771023+luhongran09@users.noreply.github.com>
2024-03-26 15:02:28 +0800

    day2

---
+++ day2-client/client.go
@@ -0,0 +1,216 @@
+package day2_client
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	day1_codec "grpc/day1-codec"
+	"grpc/day1-codec/codec"
+	"io"
+	"log"
+	"net"
+	"sync"
+)
+
+type Call struct {
+	Seq           uint64
+	ServiceMethod string
+	Args          interface{}
+	Reply         interface{}
+	Error         error
+	Done          chan *Call
+}
+
+func (call *Call) done() {
+	call.Done <- call
+}
+
+type Client struct {
+	cc       codec.Codec
+	opt      *day1_codec.Option
+	sending  sync.Mutex
+	header   codec.Header
+	mu       sync.Mutex
+	seq      uint64
+	pending  map[uint64]*Call
+	closing  bool
+	shutdown bool
+}
+
+var _ io.Closer = (*Client)(nil)
+var ErrShutdown = errors.New("connection is shut down")
+
+func (client *Client) Close() error {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	if client.closing {
+		return ErrShutdown
+	}
+	client.closing = true
+	return client.cc.Close()
+}
+func (client *Client) IsAvailable() bool {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	return !client.shutdown && !client.closing
+}
+func (client *Client) RegisterCall(call *Call) (uint64, error) {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	if client.closing || client.shutdown {
+		return 0, ErrShutdown
+	}
+	call.Seq = client.seq
+	client.pending[call.Seq] = call
+	client.seq++
+	return call.Seq, nil
+}
+func (client *Client) removeCall(seq uint64) *Call {
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	call := client.pending[seq]
+	delete(client.pending, seq)
+	return call
+}
+
+func (client *Client) terminateCalls(err error) {
+	client.sending.Lock()
+	defer client.sending.Unlock()
+	client.mu.Lock()
+	defer client.mu.Unlock()
+	client.shutdown = true
+	for _, call := range client.pending {
+		call.Error = err
+		call.done()
+	}
+}
+func (client *Client) receive() {
+	var err error
+	for err == nil {
+		var h codec.Header
+		if err = client.cc.ReadHeader(&h); err != nil {
+			break
+		}
+		call := client.removeCall(h.Seq)
+		switch {
+		case call == nil:
+			err = client.cc.ReadBody(nil)
+		case h.Error != "":
+			call.Error = fmt.Errorf(h.Error)
+			err = client.cc.ReadBody(nil)
+			call.done()
+		default:
+			err = client.cc.ReadBody(call.Reply)
+			if err != nil {
+				call.Error = errors.New("reading body" + err.Error())
+			}
+			call.done()
+		}
+	}
+}
+
+func NewClient(conn net.Conn, opt *day1_codec.Option) (*Client, error) {
+	f := codec.NewCodecFuncMap[opt.CodecType]
+	if f == nil {
+		err := fmt.Errorf("invalid codec type %s", opt.CodecType)
+		log.Println("rpc client:codec error:", err)
+		return nil, err
+	}
+	if err := json.NewEncoder(conn).Encode(opt); err != nil {
+		log.Println("rpc client : options error", err)
+		_ = conn.Close()
+		return nil, err
+	}
+	return newClientCode(f(conn), opt), nil
+}
+
+func newClientCode(cc codec.Codec, opt *day1_codec.Option) *Client {
+	client := &Client{
+		cc:      cc,
+		opt:     opt,
+		seq:     1,
+		pending: make(map[uint64]*Call),
+	}
+	go client.receive()
+	return client
+}
+
+func parseOptions(opts ...*day1_codec.Option) (*day1_codec.Option, error) {
+	if len(opts) == 0 || opts[0] == nil {
+		return day1_codec.DefaultOpion, nil
+	}
+	if len(opts) != 1 {
+		return nil, errors.New("number of options is more than 1")
+	}
+	opt := opts[0]
+	opt.MagicNumber = day1_codec.DefaultOpion.MagicNumber
+	if opt.CodecType == "" {
+		opt.CodecType = day1_codec.DefaultOpion.CodecType
+	}
+	return opt, nil
+}
+func Dial(network, address string, opts ...*day1_codec.Option) (client *Client, err error) {
+	opt, err := parseOptions(opts...)
+	if err != nil {
+		return nil, err
+	}
+	conn, err := net.Dial(network, address)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if client == nil {
+			_ = conn.Close()
+		}
+	}()
+	return NewClient(conn, opt)
+}
+func (client *Client) send(call *Call) {
+	client.sending.Lock()
+	defer client.sending.Unlock()
+	seq, err := client.RegisterCall(call)
+	if err != nil {
+		call.Error = err
+		call.done()
+		return
+	}
+	client.header.ServiceMethod = call.ServiceMethod
+	client.header.Seq = seq
+	client.header.Error = ""
+	if err := client.cc.Write(&client.header, call.Args); err != nil {
+		call := client.removeCall(seq)
+		if call != nil {
+			call.Error = err
+			call.done()
+		}
+		client.header.ServiceMethod = call.ServiceMethod
+		client.header.Seq = seq
+		client.header.Error = ""
+		if err := client.cc.Write(&client.header, call.Args); err != nil {
+			call := client.removeCall(seq)
+			if call != nil {
+				call.Error = err
+				call.done()
+			}
+		}
+	}
+}
+func (client *Client) Go(serviceMethod string, args, reply interface{}, done chan *Call) *Call {
+	if done == nil {
+		done = make(chan *Call, 10)
+	} else if cap(done) == 0 {
+		log.Panic("rpc client: done channel is unbuffered")
+	}
+	call := &Call{
+
+		ServiceMethod: serviceMethod,
+		Args:          args,
+		Reply:         reply,
+		Done:          done,
+	}
+	return call
+}
+func (client *Client) Call(serviceMethod string, args, reply interface{}) error {
+	call := <-client.Go(serviceMethod, args, reply, make(chan *Call, 1)).Done
+	return call.Error
+}
---
+++ day2-client/main/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+	day1_codec "grpc/day1-codec"
+	day2_client "grpc/day2-client"
+	"log"
+	"net"
+	"sync"
+	"time"
+)
+
+func startServer(addr chan string) {
+	l, err := net.Listen("tcp", ":0")
+	if err != nil {
+		log.Fatal("network error:", err)
+	}
+	log.Println("start rpc server on", l.Addr())
+	addr <- l.Addr().String()
+	day1_codec.Accept(l)
+}
+
+func main() {
+	log.SetFlags(0)
+	addr := make(chan string)
+	go startServer(addr)
+	client, _ := day2_client.Dial("tcp", <-addr)
+	defer func() { _ = client.Close() }()
+
+	time.Sleep(time.Second)
+
+	var wg sync.WaitGroup
+	for i := 0; i < 5; i++ {
+		wg.Add(1)
+		go func(i int) {
+			defer wg.Done()
+			args := fmt.Sprintf("geerpc req %d", i)
+			var reply string
+			if err := client.Call("Foo.Sum", args, &reply); err != nil {
+				log.Fatal("call Foo.Sum error:", err)
+			}
+			log.Println("reply:", reply)
+		}(i)
+	}
+	wg.Wait()
+}
